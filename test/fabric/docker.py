ed3f5a46cd0501b78e911f63aff548b3696ee971
Justin Phelps <linuturk@onitato.com>
2015-01-28 15:54:44 -0600

    adding artifact collection

--- test/fabric/docker.py
+++ test/fabric/docker.py
@@ -11,3 +11,26 @@ def check():
     assert file.exists("/var/run/docker.sock")
     assert process.is_up("docker")
     assert service.is_enabled("docker")
+
+
+@task
+def artifacts():
+    env.platform_family = detect.detect()
+
+    # Logs to pull
+    logs = ['/root/cfn-userdata.log',
+            '/root/heat-script.log',
+            '/tmp/heat_chef/chef-stacktrace.out']
+
+    # Artifacts target location
+    try:
+        os.environ['CIRCLE_ARTIFACTS']
+    except:
+        artifacts = 'tmp'
+    else:
+        artifacts = os.environ['CIRCLE_ARTIFACTS']
+
+    # For each log, get it down
+    for log in logs:
+        target = artifacts + "/%(host)s/%(path)s"
+        get(log, target)
